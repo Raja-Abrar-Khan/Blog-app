cc5948dfb8f508a31cb0c3f462b7bc2f9682da89
Raja Abrar Khan <abrarkhan778008@gmail.com>
2024-11-27 00:19:29 +0530

    Remove node_modules and update .gitignore

---
+++ api/index.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/user');
+const Post = require('./models/Post');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const app = express();
+const fs = require('fs');
+
+const secret = 'dfsdfsdfdsfsdsdfsdfngr'
+
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+mongoose.connect('mongodb+srv://abrarkhan778008:aodM0uAxqdj6eieU@cluster0.nxis5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
+
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedvalue = bcrypt.hashSync(password, salt);
+        const userDoc = await User.create({
+            username,
+            password: hashedvalue,
+        });
+        res.json(userDoc)
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
+
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username,
+            });
+        });
+    } else {
+        res.status(400).json('wrong credentials');
+    }
+});
+
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+})
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPAth = path + '.' + ext;
+    fs.renameSync(path, newPAth);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPAth,
+            author: info.id,
+        })
+        res.json(postDoc);
+    });
+
+
+})
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if (!isAuthor) {
+            return res.status(400).json('you are not the author')
+
+        }
+
+        await postDoc.updateOne({
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
+        });
+        res.json(postDoc);
+    });
+})
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+            .populate('author', ['username'])
+            .sort({ createdAt: -1 })
+            .limit(20)
+    );
+});
+
+app.get('/post/:id', async (req, res) => {
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+})
+
+app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+
+//aodM0uAxqdj6eieU
+//mongodb+srv://abrarkhan778008:aodM0uAxqdj6eieU@cluster0.nxis5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
